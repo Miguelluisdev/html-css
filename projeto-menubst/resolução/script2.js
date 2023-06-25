2f45b992720b1425fc723b951ed0df489fb142f9
Miguelluisdev <126804912+Miguelluisdev@users.noreply.github.com>
2023-06-25 16:11:14 -0300

    a

--- projeto-menubst/resolução/script2.js
+++ projeto-menubst/resolução/script2.js
@@ -1,4 +1,4 @@
-/*                   Show Menu            */ 
+/*=============== SHOW MENU ===============*/
 const showMenu = (toggleId, navId) =>{
   const toggle = document.getElementById(toggleId),
         nav = document.getElementById(navId)
@@ -6,7 +6,8 @@ const showMenu = (toggleId, navId) =>{
   toggle.addEventListener('click', () =>{
       // Add show-menu class to nav menu
       nav.classList.toggle('show-menu')
-      // Add show-icon to show and hide menu icon
+
+      // Add show-icon to show and hide the menu icon
       toggle.classList.toggle('show-icon')
   })
 }
